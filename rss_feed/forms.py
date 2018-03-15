ea38298cffa3870a20c83d0fdc1aa6c87b766c44
fer <fer@fer-laptop.localdomain>
2018-03-15 18:06:00 +0100

    Completing last commit

---
+++ rss_feed/forms.py
@@ -0,0 +1,19 @@
+'''
+Created on 15 Mar 2018
+
+@author: fer
+'''
+from django import forms
+from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth.models import User
+
+class SignUpForm(UserCreationForm):
+    
+    location = forms.CharField(label='Location',max_length=100,required=False)
+    description = forms.CharField(label='Description',max_length=500,required=False)
+    avatar = forms.ImageField(required=False)
+
+    class Meta:
+        model = User
+        fields = ('username', 'first_name', 'last_name', 'email', 'password1', 'password2',
+                  'location','description','avatar')
